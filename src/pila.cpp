9dbf96249c4f6cf8ecd95523ba1ec1aecfa92497
FrancoDiaz <francogregoriodiaz41@gmail.com>
2026-07-24 22:49:17 -0400

    Update pila.cpp

--- src/pila.cpp
+++ src/pila.cpp
@@ -1,1 +1,88 @@
+#include "../include/Pila.h"
 
+/*
+    Constructor
+
+    Inicializa la pila vacía.
+*/
+Pila::Pila()
+{
+    cima = -1;
+}
+
+/*
+    Inserta una nueva acción en la pila.
+
+    Limitación:
+    La pila solo admite hasta 100 elementos.
+*/
+void Pila::push(string accion)
+{
+    if (cima < 99)
+    {
+        cima++;
+        datos[cima] = accion;
+    }
+    else
+    {
+        cout << "La pila esta llena." << endl;
+    }
+}
+
+/*
+    Elimina la última acción registrada.
+
+    Limitación:
+    No puede eliminar elementos si la pila está vacía.
+*/
+void Pila::pop()
+{
+    if (!isEmpty())
+    {
+        cima--;
+    }
+    else
+    {
+        cout << "La pila esta vacia." << endl;
+    }
+}
+
+/*
+    Devuelve el elemento ubicado en la cima.
+*/
+string Pila::peek()
+{
+    if (!isEmpty())
+    {
+        return datos[cima];
+    }
+
+    return "";
+}
+
+/*
+    Indica si la pila está vacía.
+*/
+bool Pila::isEmpty()
+{
+    return cima == -1;
+}
+
+/*
+    Muestra todas las acciones almacenadas.
+*/
+void Pila::mostrar()
+{
+    if (isEmpty())
+    {
+        cout << "La pila esta vacia." << endl;
+        return;
+    }
+
+    cout << "\n===== HISTORIAL DE ACCIONES =====\n";
+
+    for (int i = cima; i >= 0; i--)
+    {
+        cout << "- " << datos[i] << endl;
+    }
+}
