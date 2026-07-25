dff99afbc16b9476892ad71849aae468ed107c8a
FrancoDiaz <francogregoriodiaz41@gmail.com>
2026-07-24 23:20:03 -0400

    Implementar constructores de la clase Producto

--- src/Producto.cpp
+++ src/Producto.cpp
@@ -1,1 +1,24 @@
+#include "../include/Producto.h"
 
+/*
+    Constructor vacío.
+*/
+Producto::Producto()
+{
+    id = 0;
+    nombre = "";
+    cantidad = 0;
+}
+
+/*
+    Constructor con parámetros.
+
+    Limitación:
+    No verifica si los datos son válidos.
+*/
+Producto::Producto(int id, string nombre, int cantidad)
+{
+    this->id = id;
+    this->nombre = nombre;
+    this->cantidad = cantidad;
+}
