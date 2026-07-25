c0d6cec2e0753c24dd0bf205e16e7a5c31c21ffd
FrancoDiaz <francogregoriodiaz41@gmail.com>
2026-07-25 00:12:26 -0400

    Crear base de la implementación del Árbol B
    Se implementó la estructura base de la clase ArbolB, incluyendo el constructor y los métodos encargados de recorrer y mostrar los productos almacenados mediante un recorrido recursivo.

--- src/ArbolB.cpp
+++ src/ArbolB.cpp
@@ -1,1 +1,58 @@
+#include "../include/ArbolB.h"
 
+/*
+    Constructor del Árbol B.
+
+    Inicializa el árbol sin nodos.
+*/
+ArbolB::ArbolB()
+{
+    raiz = nullptr;
+}
+
+/*
+    Muestra todos los productos almacenados
+    en el Árbol B.
+*/
+void ArbolB::mostrar()
+{
+    if (raiz == nullptr)
+    {
+        cout << "El Árbol B está vacío." << endl;
+        return;
+    }
+
+    mostrarRecursivo(raiz);
+}
+
+/*
+    Recorre el árbol de forma recursiva
+    mostrando los productos almacenados.
+*/
+void ArbolB::mostrarRecursivo(NodoB* nodo)
+{
+    if (nodo == nullptr)
+    {
+        return;
+    }
+
+    int i;
+
+    for (i = 0; i < nodo->cantidadProductos; i++)
+    {
+        if (!nodo->esHoja)
+        {
+            mostrarRecursivo(nodo->hijos[i]);
+        }
+
+        cout << "ID: " << nodo->productos[i].id
+             << " | Nombre: " << nodo->productos[i].nombre
+             << " | Cantidad: " << nodo->productos[i].cantidad
+             << endl;
+    }
+
+    if (!nodo->esHoja)
+    {
+        mostrarRecursivo(nodo->hijos[i]);
+    }
+}
