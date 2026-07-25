8b7c224acaf2efa3c1303c9dd21ff3f9d1bef34d
FrancoDiaz <francogregoriodiaz41@gmail.com>
2026-07-24 22:33:10 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+    cout << "==========================================" << endl;
+    cout << "        SMARTSHOP" << endl;
+    cout << " Lista de Compras Interactiva" << endl;
+    cout << " Proyecto de Estructuras de Datos" << endl;
+    cout << " Grupo Gamma" << endl;
+    cout << "==========================================" << endl;
+
+    cout << "\nSistema iniciado correctamente." << endl;
+
+    return 0;
+}
