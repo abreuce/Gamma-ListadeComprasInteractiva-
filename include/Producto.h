d2e2fb639556e35b40b87699ac83035a98c4f05b
FrancoDiaz <francogregoriodiaz41@gmail.com>
2026-07-24 23:19:37 -0400

    Agregar definición de la clase Producto
    Added Producto class to represent products in SmartShop.

--- include/Producto.h
+++ include/Producto.h
@@ -1,1 +1,36 @@
+#ifndef PRODUCTO_H
+#define PRODUCTO_H
 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/*
+    Clase Producto
+
+    Representa un producto dentro de SmartShop.
+
+    Será utilizada por las listas enlazadas, árboles,
+    hashing y grafos durante el desarrollo del proyecto.
+
+    Limitación:
+    No valida si los datos ingresados son correctos.
+*/
+
+class Producto
+{
+public:
+
+    int id;
+    string nombre;
+    int cantidad;
+
+    // Constructor por defecto
+    Producto();
+
+    // Constructor con parámetros
+    Producto(int id, string nombre, int cantidad);
+};
+
+#endif
