cd9a11a71eefc6fafc63a5bfb4265cb199bdeaa6
FrancoDiaz <francogregoriodiaz41@gmail.com>
2026-07-24 23:46:16 -0400

    Agregar estructura NodoB para el Árbol B
    Se creó la clase NodoB, encargada de almacenar los productos y las referencias a los nodos hijos. Esta estructura será utilizada como base para la implementación del Árbol B.

--- include/NodoB.h
+++ include/NodoB.h
@@ -1,1 +1,47 @@
+#ifndef NODOB_H
+#define NODOB_H
 
+#include "Producto.h"
+
+/*
+    Clase NodoB
+
+    Representa un nodo de un Árbol B.
+
+    Cada nodo puede almacenar varias claves (productos)
+    y referencias a sus nodos hijos.
+
+    Esta estructura servirá como base para implementar
+    el Árbol B de SmartShop.
+
+    Limitaciones:
+    - No implementa eliminación.
+    - La división de nodos será gestionada por la clase ArbolB.
+*/
+
+class NodoB
+{
+public:
+
+    static const int ORDEN = 3;
+
+    Producto productos[ORDEN - 1];
+    NodoB* hijos[ORDEN];
+
+    int cantidadProductos;
+    bool esHoja;
+
+    // Constructor
+    NodoB(bool hoja)
+    {
+        esHoja = hoja;
+        cantidadProductos = 0;
+
+        for (int i = 0; i < ORDEN; i++)
+        {
+            hijos[i] = nullptr;
+        }
+    }
+};
+
+#endif
