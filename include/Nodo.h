67d0ceed074770ea0ea29df430a56678bbd3046b
FrancoDiaz <francogregoriodiaz41@gmail.com>
2026-07-24 23:23:47 -0400

    Agregar estructura Nodo para la lista enlazada

--- include/Nodo.h
+++ include/Nodo.h
@@ -1,1 +1,34 @@
+#ifndef NODO_H
+#define NODO_H
 
+#include "Producto.h"
+
+/*
+    Clase Nodo
+
+    Representa un nodo de la lista enlazada.
+
+    Cada nodo almacena un producto y un apuntador
+    al siguiente nodo de la lista.
+
+    Limitación:
+    Solo permite enlazar un nodo siguiente
+    (lista simplemente enlazada).
+*/
+
+class Nodo
+{
+public:
+
+    Producto producto;
+    Nodo* siguiente;
+
+    // Constructor
+    Nodo(Producto producto)
+    {
+        this->producto = producto;
+        siguiente = nullptr;
+    }
+};
+
+#endif
