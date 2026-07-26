db62352fc434d2a2b696a9de75ed6783f8b49ed8
FrancoDiaz <francogregoriodiaz41@gmail.com>
2026-07-26 15:15:41 -0400

    Definir la estructura base de la Tabla Hash
    Se creó el archivo HashTable.h con la definición de la clase HashTable, incluyendo la función hash, la estructura basada en vector y listas para el manejo de colisiones, y los métodos públicos para insertar, buscar y mostrar productos.

--- include/HashTable.h
+++ include/HashTable.h
@@ -1,1 +1,24 @@
+#ifndef HASHTABLE_H
+#define HASHTABLE_H
 
+#include <vector>
+#include <list>
+#include "Producto.h"
+
+class HashTable
+{
+private:
+    static const int TAM = 10;
+    std::vector<std::list<Producto>> tabla;
+
+    int funcionHash(int id);
+
+public:
+    HashTable();
+
+    void insertar(const Producto& producto);
+    Producto* buscar(int id);
+    void mostrar();
+};
+
+#endif
