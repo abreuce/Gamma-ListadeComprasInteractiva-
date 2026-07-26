fe1f78e93795411ae0fb12d9c5015feba5999e16
FrancoDiaz <francogregoriodiaz41@gmail.com>
2026-07-26 15:59:03 -0400

    Definir la estructura base del Grafo
    Se creó el archivo Grafo.h con la definición de la clase Grafo, utilizando una matriz de adyacencia para representar las conexiones entre vértices. Se declararon los métodos necesarios para agregar vértices, conectar nodos, mostrar la estructura y realizar un recorrido en profundidad (DFS).

--- include/Grafo.h
+++ include/Grafo.h
@@ -1,1 +1,43 @@
+#ifndef GRAFO_H
+#define GRAFO_H
 
+#include <vector>
+#include <string>
+
+using namespace std;
+
+/*
+    Clase Grafo
+
+    Representa un grafo no dirigido utilizando
+    una matriz de adyacencia.
+
+    Permite agregar conexiones entre vértices
+    y realizar un recorrido DFS.
+
+    Limitación:
+    El tamaño del grafo se define al crearlo
+    y no cambia dinámicamente.
+*/
+
+class Grafo
+{
+private:
+    int numVertices;
+    vector<vector<int>> matriz;
+    vector<string> nombres;
+
+    void DFSRecursivo(int vertice, vector<bool>& visitado);
+
+public:
+    Grafo(int vertices);
+
+    void agregarVertice(int indice, string nombre);
+    void agregarArista(int origen, int destino);
+
+    void mostrar();
+
+    void DFS(int inicio);
+};
+
+#endif
